917cbefae6344fd3d87ad8f300ef6d4ab2a69954
Roger Florat <rflorat@cidiverte.it>
2018-01-22 15:19:07 +0100

    GOALPOST Layout

---
+++ GoalPost/GoalPost/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  GoalPost
+//
+//  Created by Roger Florat on 22/01/18.
+//  Copyright © 2018 Roger Florat. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    // Outlet
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addGoalBtnWasPressed(_ sender: Any) {
+        print("Button was pressed")
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# APP - GOALPOST ( IOS 11 - Swift 4 )
+
+1. Core Data
